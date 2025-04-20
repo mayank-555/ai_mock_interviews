45d3009cadb20e3dcb4391b38e77fceccb46fcc0
Your Name <you@example.com>
2025-04-20 17:18:04 +0530

    show generated interviews on homepage

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,7 +1,9 @@
 import Agent from '@/components/Agent'
+import { getCurrentUser } from '@/lib/actions/auth.action';
 import React from 'react'
 
-const Page = () => {
+const Page = async() => {
+  const user = await getCurrentUser();
   return (
     <>
     <h3>Interview Generation</h3>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -101,3 +101,6 @@ export async function isAuthenticated(){
     const user = await getCurrentUser();
     return !!user;
 }
+
+
+
